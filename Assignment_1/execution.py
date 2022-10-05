9eab0e214d65f4a1af019e776ce97891d4b44644
MichaelPieke <71767600+MichaelPieke@users.noreply.github.com>
2022-10-05 17:47:13 +0200

    Add files via upload
    Added comments and organised functions. Changed the execution file slightly, as parameters of tree_pred were the wrong way around

--- Assignment_1/execution.py
+++ Assignment_1/execution.py
@@ -2,18 +2,16 @@ from symbol import for_stmt
 import numpy as np
 from functions import *
 from sklearn.model_selection import train_test_split
-credit_data = np.genfromtxt(
-    'Assignment_1/credit.txt', delimiter=',', skip_header=True)
+credit_data = np.genfromtxt('credit.txt', delimiter=',', skip_header=True)
 
 x = credit_data[:, 0: credit_data.shape[1] - 1]
 y = credit_data[:, credit_data.shape[1] - 1]
 y = np.reshape(y, (len(y), 1))
 # checked to see if the same as slides. It is the same, only leaf nodes are duplicated (i.e. parent of leaf == leaf). Probably good to fix.
-tree = tree_grow(x, y, 2, 1, 8)
-print(RenderTree(tree))
-print(tree_pred(x, tree))
-trees = tree_grow_b(x=x, y=y, nmin=2, minleaf=3, nfeat=8, m=5)
-print(trees)
-print(tree_pred_b(trees, x))
+#tree = tree_grow(x, y, 2, 1, x.shape[1])
+trees = tree_grow_b(x, y, 15, 5, 41, m=5)
+#print(RenderTree(trees[3]))
+#print(trees)
+print(tree_pred_b(x, trees))
 # print(x)
 # print(y)
