12369d38d27d3d586a92dd4cb04df7fc371c04b9
MichaelPieke <71767600+MichaelPieke@users.noreply.github.com>
2022-10-12 14:59:02 +0200

    Add files via upload
    updated minleaf criteria for better random forest

---
+++ analysis.py
@@ -0,0 +1,127 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  7 14:51:44 2022
+
+@author: scro0
+"""
+
+#Part 2: analysis
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from functions import *
+from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix
+import time
+from anytree.exporter import DotExporter
+
+#Get the relevant features from bug dataset
+def slice_data(data):
+  x = data.loc[:, 'ACD_avg':'VG_sum']
+  pre = data.loc[:,'pre']
+  x = np.array(pd.concat([pre, x], axis = 1))
+  y = np.array(data.loc[:,'post'])
+  
+  for i in range(len(y)): #binarise labels
+      if y[i] > 1:
+          y[i] = 1
+         
+  y = np.reshape(y, (y.shape[0], 1))
+
+  return x, y
+
+x_train, y_train = slice_data(pd.read_csv('eclipse-metrics-packages-2.0.csv', delimiter = ';'))
+x_test, y_test = slice_data(pd.read_csv('eclipse-metrics-packages-3.0.csv', delimiter = ';'))
+
+'''
+Train a single classification tree on the training set with nmin = 15,
+minleaf = 5 (we have pre-selected reasonable values for you), and nfeat
+= 41. Compute the accuracy, precision and recall on the test set.
+
+'''
+
+def results(y_test, y_pred, time_1, time_2, model_name):
+    print(f'Time elapsed for {model_name}: {abs(time_1 - time_2)} seconds')
+    print(f'Accuracy: {accuracy_score(y_test, y_pred)}')
+    print(f'Precision: {precision_score(y_test, y_pred)}')
+    print(f'Recall: {recall_score(y_test, y_pred)}')
+    cf = confusion_matrix(y_test, y_pred)
+    print(cf)
+    print()
+    
+    sns.heatmap(cf, annot=True).set(title="Confusion matrix of " + model_name)
+
+
+
+#Model 1
+
+time_1 = time.perf_counter()
+model_1 = tree_grow(x_train, y_train, nmin = 15, minleaf = 5, nfeat = 41)
+y_pred_1 = tree_pred(x_test, model_1)
+time_2 = time.perf_counter()
+results(y_test, y_pred_1, time_1, time_2, "single tree")
+
+#Model 2
+time_1 = time.perf_counter()
+model_2 = tree_grow_b(x_train, y_train, nmin = 15, minleaf = 5, nfeat = 41, m = 100)
+y_pred_2 = tree_pred_b(x_test, model_2)
+time_2 = time.perf_counter()
+results(y_test, y_pred_2, time_1, time_2, "bagging model")
+
+#Model 3
+time_1 = time.perf_counter()
+model_3 = tree_grow_b(x_train, y_train, nmin = 15, minleaf = 5, nfeat = 6, m = 100)
+y_pred_3 = tree_pred_b(x_test, model_3)
+time_2 = time.perf_counter()
+results(y_test, y_pred_3, time_1, time_2, "random forest")
+
+
+
+credit_data = np.genfromtxt("credit.txt", delimiter = ',', skip_header = True)
+x = credit_data[:, 0:credit_data.shape[1]-1]
+y = credit_data[:,credit_data.shape[1]-1]
+y = np.reshape(y, (len(y), 1))
+model_test = tree_grow(x, y, nmin = 0, minleaf = 0, nfeat = 41)
+y_pred_test = tree_pred(x, model_test)
+y_pred_test = np.reshape(y_pred_test, (len(y_pred_test), 1))
+false_preds = np.where(y_pred_test != y)
+print(false_preds)
+false_labels = y[false_preds[0]]
+false_preds = x[false_preds[0]]
+
+#results(y_train, y_pred_1, time_1, time_2, "single tree")
+
+
+#This is to test if the DT code works for pima dataset
+
+
+pima = np.genfromtxt('pima.txt', delimiter = ',')
+
+x = pima[:, 0:pima.shape[1]-1]
+y = pima[:, pima.shape[1]-1]
+y = np.reshape(y, (768,1))
+time_1 = time.perf_counter()
+model_1 = tree_grow(x, y, nmin = 20, minleaf = 5, nfeat = 8)
+y_pred_1 = tree_pred(x, model_1)
+time_2 = time.perf_counter()
+results(y, y_pred_1, time_1, time_2, "RF")
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+dt = DecisionTreeClassifier(criterion = 'gini', min_samples_split = 15, min_samples_leaf = 5)
+
+dt_pima = dt.fit(x_train, y_train).predict(x_test)
+print(confusion_matrix(y_test, dt_pima))
+print(f'Accuracy: {accuracy_score(y_test, dt_pima)}')
+
+print(confusion_matrix(y_test, y_pred_1))
+print(f'Accuracy: {accuracy_score(y_test, y_pred_1)}')
+
+
+rf = RandomForestClassifier(n_estimators = 100, criterion = 'gini', min_samples_split = 15, min_samples_leaf = 5, max_features = 41)
+print(f'Accuracy: {accuracy_score(y_test, rf.fit(x_train, y_train).predict(x_test))}')
+print(confusion_matrix(y_test, rf.fit(x_train, y_train).predict(x_test)))
+print(confusion_matrix(y_test, y_pred_3))
+
+#for random forest, some trees seem to always output 1, which heavily makes the predictions 1
---
+++ functions.py
@@ -0,0 +1,315 @@
+
+# Assignment 1 ~ Classification Trees, Bagging and Random Forest
+# Rosalie Lucas 6540384
+# Michael Pieke 8474752
+# Mick Richters 6545572
+
+# Part 1: Programming
+
+# Import necessary libraries
+import pandas as pd
+import random
+from anytree import Node, RenderTree
+from collections import Counter
+import numpy as np
+
+#################################################### MAIN FUNCTIONS ###########################################################
+
+
+# Create a single DT
+def tree_grow(x, y, nmin, minleaf, nfeat):
+    # build_tree is used as auxiliary function in order for tree_grow to have the same paramaters as in the assignment
+    root = Node("root", parent=None, question=Question(None, None, None)) #root node to be expanded
+    return build_tree(root, x, y, nmin, minleaf, nfeat)
+
+# Predict labels for x-values given a single decision tree
+
+
+def tree_pred(x, tr):
+    predicted_y = []  # Safe predictions here
+    features = range(x.shape[1])  # Define features index
+
+    # Predict for each x
+    for row in x:
+        # Get class from the tree
+        predicted_y.append(get_decision(row, tr, features))
+
+    # Return array with predictions
+    return np.array(predicted_y)
+
+# Grow a forest of decision trees
+
+
+def tree_grow_b(x, y, nmin, minleaf, nfeat, m):
+    # Create forest of m trees
+    forest = []  # Safe the trees in here
+    tester_x = None
+    #test = []
+    for i in range(m):
+        x, y = bootstrap(x, y, len(x))  # Get bootstrapped sample
+        tree = tree_grow(x, y, nmin=nmin, minleaf=minleaf,
+                         nfeat=nfeat)  # Grow a single tree based on bootstrapped sample
+        pred = tree_pred(x, tree)
+        #print(pred)
+        if np.all(pred == 1):
+            print('yes')
+            #test.append(tree)
+        if  np.all(pred == 0):
+            print('yes')
+            #test.append(tree)
+        forest.append(tree)
+
+    return forest
+
+
+# Make prediction for y values from the forest
+def tree_pred_b(x, forest):
+    # Create variables to safe the predictions
+    tree_preds = np.zeros((0, len(x)))
+    predictions = []
+
+    # For each tree in the forest, make predictions for y values
+    for tree in forest:
+        tree_preds = np.append(tree_preds, [tree_pred(x, tree)], axis=0)
+
+    # Transpose the predictions to get majority vote
+    tree_preds = np.transpose(tree_preds)
+    
+
+    # Calculate majority vote over all trees for each x value (with binary data, majority vote == average value)
+    for i in range(len(tree_preds)):
+        majority_vote = Counter(tree_preds[i]).most_common(1)[0][0]
+        # Final predictions of the random forest
+        predictions.append(majority_vote)
+
+    return predictions
+
+################################################ AUXILIARY FUNCTIONS FOR TREE_GROW ###########################################################
+
+# The real building
+
+
+def build_tree(node, x, y, nmin, minleaf, nfeat):
+    # Base Case: stop expanding if node is pure
+    #TODO: voeg beide stopping criteria 
+    
+    #TODO fix nonetype error!
+    
+    if nfeat < x.shape[1]:  # get random subset of features of size nfeat
+        features = random.sample(range(x.shape[1]), nfeat)
+
+    else:  # Use all features
+        features = range(x.shape[1])
+        
+    split, feature, gain = best_split(x, y, features, minleaf)
+    
+    if gain == 0 or len(x) < nmin or split == None: #if split == None, no minleaf criteria have been met
+        y_temp = np.reshape(y, (y.shape[0],))  # Reshape to fit counter
+        majority_class = Counter(y_temp.tolist()).most_common(1)[0][0]
+        #majority_class = y[0][0]
+        #print(majority_class)
+        #print()
+        # Get final class value, should be the same for all y in Y as node is pure. Hence, we can select the first element of y.
+        
+        #does this make sense with question etc?
+
+        # TODO: Not sure if this will duplicate the same node and can't access the parent nodes to change them
+        leaf = Node("leaf " + str(majority_class), parent=node, question=Question(None, None, None))
+        leaf.question.label = majority_class
+        #node.children = [leaf]
+        #node.name = "leaf " + str(majority_class)
+        return leaf
+    
+    
+
+    # Return the best value to split on as well as the corresponding feature (column index)
+    
+
+    # Partition the data according to the best_split
+    lhs_x, lhs_y, rhs_x, rhs_y, question = partition(feature, split, x, y)
+
+    lhs_y = np.reshape(lhs_y, (lhs_y.shape[0], 1))  # Reshape to fit the format
+    rhs_y = np.reshape(rhs_y, (rhs_y.shape[0], 1))  # Reshape to fit the format
+
+    # Early Stopping Criteria (as provided in the assignment)
+    '''if (len(x) < nmin):
+        #print("early stopping")
+        #print(y)
+        y_temp = np.reshape(y, (y.shape[0],))  # Reshape to fit counter
+        majority_class = Counter(y_temp.tolist()).most_common(1)[0][0]
+        #print(majority_class)
+        #print()
+
+        # Create leaf node with majority class label
+        question.label = majority_class
+        leaf = Node("leaf " + str(majority_class), parent=node, question=question)
+        node.children = [leaf]
+
+        return leaf'''
+
+    # Expand left and right side of tree using recursion
+    node_lhs = build_tree(Node("lhs " + str(question.feature) + " value: " + str(np.round(question.value, 3)), parent=node, question=question),
+                          lhs_x, lhs_y, nmin, minleaf, nfeat)
+    node_rhs = build_tree(Node("rhs " + str(question.feature) + " value: " + str(np.round(question.value, 3)), parent=node, question=question),
+                          rhs_x, rhs_y, nmin, minleaf, nfeat)
+
+    node.children = [node_lhs, node_rhs]
+    node.question = question
+
+    # When finished building, return node
+    return node
+
+
+# Calculate impurity using Gini-Index
+def impurity(y):
+    if len(y) > 0:
+        prob1 = sum(y) / len(y)
+        prob2 = 1 - prob1
+        return prob1*prob2
+
+    else:
+        return 0
+
+
+# Calculate impurity reduction for a node
+def impurity_reduction(y, lh, rh):
+    propl = len(lh) / len(y)
+    propr = len(rh) / len(y)
+    imp = impurity(y)
+    reduction = imp - ((propl * impurity(lh)) + (propr * impurity(rh)))
+
+    return reduction
+
+# Find the split which provides highest information gain
+
+
+def best_split(x, y, features, minleaf):
+    # Set default values
+    best_gain = 0
+    split_feature = None
+    best_split = None
+
+    # Loop through all features to define the best feature to split on
+    for feature in features:
+        column = x[:, feature]
+        x_sorted = np.sort(np.unique(column))
+
+        # Define the splitpoints
+        splitpoints = (x_sorted[0:(len(x_sorted)-1)] +
+                       x_sorted[1:len(x_sorted)])/2
+        # Calculate the impurity reduction for each split point
+        for splitpoint in splitpoints:
+            lh = y[column < splitpoint]
+            rh = y[column >= splitpoint]
+            if len(lh) == 0 or len(rh) == 0:
+                continue
+            
+            gain = impurity_reduction(y, lh, rh)
+
+            # Only update best gain if minleaf constraint is met
+            if (gain > best_gain) and (len(lh) >= minleaf) and (len(rh) >= minleaf)  :
+                best_gain = gain
+                best_split = splitpoint
+                split_feature = feature
+
+    # Return the value and feature for the best split
+    return best_split, split_feature, best_gain
+
+
+# Partition based on given feature and split value
+def partition(feature, split, x, y):
+    question = Question(feature, split)
+    # Easier to get correct labels for split data
+    x = np.concatenate((x, y), axis=1)
+    # Split data into left and right hand sides
+    
+    lhs = x[x[:,feature] < split]
+    #np.array([instance for instance in x if instance[feature] < split])
+    #rhs = np.array([instance for instance in x if instance[feature] >= split])
+    rhs = x[x[:,feature] >= split]
+    '''except:
+        print(x[:,feature])
+        print(split)
+        print(x[:,feature] < split)'''
+    columns = x.shape[1]-1
+
+    # Return right and left side x and y values to use for next iteration of build_tree function
+    return lhs[:, 0: columns], lhs[:, columns], rhs[:, 0: columns], rhs[:, columns], question
+
+# TODO: add documentation https://www.youtube.com/watch?v=LDRbO9a6XPU
+
+
+#################################################### AUXILIARY FUNCTIONS FOR TREE_PRED ###########################################################
+
+# Get the final class for a leaf node
+def get_decision(row, tr, features):
+    decision = None
+
+    # Loop through each feature to see which one matches the current node
+    while True:
+        # Check if root node is leaf node
+        if type(tr) == list:
+            print(tr)
+        if tr.question.feature == None:
+            return tr.question.label
+
+        # Match feature with child feature
+        #for child in tr.children:
+         #   print(child.name)
+        #print()
+        for feat in features:
+            if (tr.question.feature == feat):
+                # If feature value >= value in question, get right hand of tree
+                if (tr.question.answer(row)):
+                    tr = tr.children[1]
+
+                # Otherwise get left hand of tree
+                else:
+                    tr = tr.children[0]
+
+            # Check for leaf node and return correct class
+            if tr.name.startswith("leaf"):
+                decision = tr.question.label
+                return int(decision)
+
+# Get child of a node
+
+
+def get_child(node, index):
+    # If node is a leaf, return node
+    return node.children[index]
+    #for child in node.children:
+        #if (child.name == child_name):
+        #    return child
+        #if  (child.name == "leaf"):
+            #return child
+
+
+# Auxiliary class to store information based on which the data was split at each iteration of build_tree
+class Question:  # TODO: documentation inspired by this tutorial: https://github.com/random-forests/tutorials/blob/master/decision_tree.ipynb
+
+    def __init__(self, feature, value, label=None):
+        # Question stores the feature, value and label of a node
+        self.feature = feature
+        self.value = value  # TODO: this value is the right hand side of the boolean, the left hand side is the feature value of the example
+        self.label = label
+
+    def answer(self, instance):
+        val = instance[self.feature]
+
+        # Return whether value is bigger than split value or not
+        return val >= self.value
+
+
+################################################ AUXILIARY FUNCTION FOR RANDOM FOREST ###########################################################
+
+
+# Get a bootstrap sample from the data
+def bootstrap(X, Y, n_bootstraps):
+    bootstrap_indices = np.random.randint(
+        low=0, high=len(X), size=n_bootstraps)
+    df_bootstrapped_x = X[bootstrap_indices]
+    df_bootstrapped_y = Y[bootstrap_indices]
+
+    # Return the bootstrapped x and y values
+    return df_bootstrapped_x, df_bootstrapped_y
